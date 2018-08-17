c1a0566cde4e520e1426694cd01a79d87c0b8426
Your Name <you@example.com>
2018-08-17 15:56:18 +0300

    added env variables

--- order_management.py
+++ order_management.py
@@ -1,6 +1,15 @@
+# coding: utf8
+import shopify
+from flask import Flask, request, redirect
 import os
 
-from flask import Flask, request
+API_KEY = os.environ["API_KEY"]
+PASSWORD = os.environ["PASSWORD"]
+SHOP_URL = os.environ["SHOP_URL"]
+shop_url =  "https://%s:%s@%s/admin" % (API_KEY, PASSWORD, SHOP_URL)
+
+shopify.ShopifyResource.set_site(shop_url)
+shop = shopify.Shop.current()
 app = Flask(__name__)
 
 
@@ -12,7 +21,7 @@ def hello_world():
 def orders_manage():
     if request.method == 'POST':
         print(request.form)
-        return request.form
+        return redirect("http://www.google.com", code=302)
     else:
         return "asdasd"
     return "v"
