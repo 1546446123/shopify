b7ae458603355ae0344bccaa796f6e3ed277a553
Your Name <you@example.com>
2018-08-17 12:01:55 +0300

    added os env var

--- order_management.py
+++ order_management.py
@@ -1,9 +1,13 @@
+import os
+
 from flask import Flask
 app = Flask(__name__)
 
+s = os.environ['MY']
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return s
 
 if __name__ == '__main__':
     app.run()
